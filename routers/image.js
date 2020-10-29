65e32f219cb8a96184c7835bc91f37a5660de28c
jurvantoledo <jurvantoledo@live.nl>
2020-10-29 12:44:15 +0100

    added routers with working post

--- routers/image.js
+++ routers/image.js
@@ -1,8 +1,26 @@
 const { Router } = require("express")
 const router = new Router()
 
+const Image = require("../models").images
+
 router.get("/", (req, res) => {
-    res.send()
+    res.send(req.body)
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { title, url } = req.body
+        if(!title || !url) {
+            res.status(400).send("missing parameters")
+        }
+        const newImage = await Image.create({
+            title,
+            url
+        })
+        res.json(newImage)
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = router
--- routers/user.js
+++ routers/user.js
@@ -1,8 +1,29 @@
 const { Router } = require("express")
 const router = new Router()
+const bcrypt = require('bcrypt')
+
+const User = require("../models").user
 
 router.get("/", (req, res) => {
-    res.send()
+    res.send(req.body)
+})
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { email, password, fullName } = req.body
+        if(!email || !password || !fullName) {
+            res.status(400).send("missing parameters")
+        }
+        const hashedPassword = bcrypt.hashSync(password, 10)
+        const newUser = await User.create({
+            email,
+            password: hashedPassword,
+            fullName
+        })
+        res.json(newUser)
+    } catch (e) {
+        next(e)
+    }
 })
 
 module.exports = router

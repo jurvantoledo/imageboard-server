f5dbbea56c2f7633f71b573f68c39a33606c2b0d
jurvantoledo <jurvantoledo@live.nl>
2020-10-29 17:32:46 +0100

    modified login

--- routers/auth.js
+++ routers/auth.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { toJWT, toData } = require('../auth/jwt')
+const bcrypt = require('bcrypt')
 
 const User = require('../models').user
 
@@ -13,9 +14,29 @@ try {
             message: "Please supply a valid email and password",
           });
     }
+    // 1. find user based on email address
+const user = await User.findOne({
+    where: {
+      email: email,
+    },
+  });
+  if (!user) {
+    res.status(400).send({
+      message: "User with that email does not exist",
+    });
+  }
+  // 2. use bcrypt.compareSync to check the password against the stored hash
+  else if (bcrypt.compareSync(password, user.password)) {
+    // 3. if the password is correct, return a JWT with the userId of the user (user.id)
+    const jwt = toJWT({ userId: user.id });
     res.send({
-        jwt: toJWT({ userId: 1 }),
-      });
+      jwt,
+    });
+  } else {
+    res.status(400).send({
+      message: "Password was incorrect",
+    });
+  }
 } catch (e) {
     next(e)
 }

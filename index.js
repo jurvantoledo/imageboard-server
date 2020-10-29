90e7296a9ce3f8f4e1af5d8d3597d4b0c0402baf
jurvantoledo <jurvantoledo@live.nl>
2020-10-29 11:41:15 +0100

    added routes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express")
+const imageRouter = require("./routers/image")
+const userRouter = require("./routers/user")
 
 const app = express()
+const jsonParser = express.json()
+
+app.use(jsonParser)
+
+app.use("/images", imageRouter)
+app.use("/users", userRouter)
 
 const PORT = process.env.PORT | 4000
-
 app.listen(PORT, console.log(`Listening on: ${PORT}`))
--- models/image.js
+++ models/image.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class images extends Model {
+  class image extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,12 +13,12 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  images.init({
+  image.init({
     title: DataTypes.STRING,
     url: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'images',
   });
-  return images;
+  return image;
 };
